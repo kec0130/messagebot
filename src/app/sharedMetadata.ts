3362f9ba4fb7f26ba434ace4f82325253e490249
eunchae <kec0130@gmail.com>
2023-09-29 01:32:38 +0900

    fix: add metadata url

--- src/app/sharedMetadata.ts
+++ src/app/sharedMetadata.ts
@@ -1,14 +1,12 @@
 import { Metadata } from 'next';
 
 interface Props {
-  url: string;
   title?: string;
   description?: string;
   keywords?: string[];
 }
 
 const sharedMetadata = {
-  url: 'https://messagebot.chaechae.life',
   title: '메시지봇 - 인사말 생성 AI 챗봇',
   description:
     '소중한 사람들에게 따뜻한 인사말을 전하세요. 생일 축하, 명절 인사, 축하 메시지, 안부 메시지를 AI가 쉽고 빠르게 만들어드립니다.',
@@ -30,7 +28,6 @@ const sharedMetadata = {
 };
 
 export const getMetaData = ({
-  url,
   title = sharedMetadata.title,
   description = sharedMetadata.description,
   keywords = sharedMetadata.keywords,
@@ -38,4 +35,5 @@ export const getMetaData = ({
   title,
   description,
   keywords,
+  metadataBase: new URL('https://messagebot.chaechae.life'),
 });
