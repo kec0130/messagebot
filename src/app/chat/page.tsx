6fce703dba07c28f0405c86841a0e506b9216282
eunchae <kec0130@gmail.com>
2023-09-29 01:35:21 +0900

    fix: fix metadata props

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 import { getMetaData } from '../sharedMetadata';
 import ChatRoom from '@/components/Chat';
 
-export const metadata = getMetaData({ url: '/chat' });
+export const metadata = getMetaData({});
 
 const Chat = () => {
   return <ChatRoom />;
