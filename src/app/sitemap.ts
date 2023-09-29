336e624f9c1dfdef540bfe37938a3f95d1daa39c
eunchae <kec0130@gmail.com>
2023-09-29 17:02:21 +0900

    chore: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://messagebot.chaechae.life',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://messagebot.chaechae.life/chat',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
