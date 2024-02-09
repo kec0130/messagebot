336f737ad398d142e01c3b58c95069d580cae12e
eunchae <kec0130@gmail.com>
2024-02-09 16:49:50 +0900

    Update footer

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -4,22 +4,25 @@ const Footer = () => {
   return (
     <footer className="footer footer-center bg-base-200 p-10 text-base-content">
       <aside>
-        <Image src="/images/text-logo.png" alt="logo" width={60} height={17} className="mb-2" />
+        <Image src="/images/text-logo.png" alt="logo" width={70} height={20} className="mb-1" />
+        <p>&copy; {new Date().getFullYear()} ChaeChae. All Rights Reserved.</p>
+      </aside>
+      <nav>
+        <header className="footer-title mb-0 font-sans">About Us</header>
         <a
           href="https://chaechae.life"
           target="_blank"
           rel="noreferrer noopener"
           className="link-hover link"
         >
-          About us
+          개발자 홈페이지
         </a>
         <a href="mailto:chaechae.couple@gmail.com" className="link-hover link">
           chaechae.couple@gmail.com
         </a>
-        <p>&copy; {new Date().getFullYear()} ChaeChae. All Rights Reserved.</p>
-      </aside>
+      </nav>
       <nav>
-        <header className="footer-title font-sans">Family Site</header>
+        <header className="footer-title mb-0 font-sans">Family Site</header>
         <a
           href="https://ev-charge.kr"
           target="_blank"
@@ -28,14 +31,6 @@ const Footer = () => {
         >
           전기차G - 전기차 충전소 찾기
         </a>
-        {/* <a
-          href="https://chaechaekorea.com"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="link-hover link"
-        >
-          ChaeChae Korea
-        </a> */}
       </nav>
     </footer>
   );
